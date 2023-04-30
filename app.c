6c5f4a838650ee119ed6c10e72f6982209463092
lyt112233 <130142057+lyt112233@users.noreply.github.com>
2023-04-30 17:21:40 +0800

    Add files via upload

---
+++ app.c
@@ -0,0 +1,13 @@
+#include "other1.h"
+#include "other2.h"
+#include <stdio.h>
+void main(){
+        int a=15,b=3,c;
+        printf( "test in app\n" );
+        welcome();
+        c = add(a, b);
+        printf( "%d - %d = %d\n", a, b, c );
+        c = sub(a, b);
+        printf( "%d - %d = %d\n", a, b, c );
+}
+
